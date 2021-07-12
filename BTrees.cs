cb0217e5124b7cbfb23aa8010924ea99dc988737
Your Name <you@example.com>
2021-07-12 16:19:46 +0200

    add init

---
+++ BTrees.cs
@@ -0,0 +1,132 @@
+﻿using System;
+
+namespace BTrees
+{
+    public class BTree
+    {
+        Node root;
+
+        public BTree(int data) { root = new Node(data); }
+
+        public void Insert(int data) {
+            if (root != null) {
+                root.Insert(data);
+            } else {
+                root = new Node(data);
+            }
+        }
+
+        public Node Find(int data) {
+            return root != null ? root.Find(data) : null;
+        }
+
+        // O(Log n)
+        public Node FindRecursive(int data) {
+            return root != null ? root.FindRecursive(data) : null;
+        }
+
+        public void Remove(int data) {
+            Node current = root;
+            Node parent = root;
+            bool isLeft = false;
+
+            if (current == null) {
+                return;
+            }
+
+            while (current != null && current.data != data) {
+                parent = current;
+
+                if (data < current.data) {
+                    current = current.left;
+                    isLeft = true;
+                } else {
+                    current = current.right;
+                    isLeft = false;
+                }
+            }
+
+            // if node not found
+            if (current == null) {
+                return;
+            }
+
+            // check if left or right child node
+            if (current.right == null && current.left == null) {
+                if (current == root) {
+                    root = null;
+                } else {
+                    if (isLeft) {
+                        parent.left = null;
+                    } else {
+                        parent.right = null;
+                    }
+                }
+            } else if (current.right == null) {
+                if (current == root) {
+                    root = current.left;
+                } else {
+                    if (isLeft) {
+                        parent.left = current.left;
+                    } else {
+                        parent.right = current.left;
+                    }
+                }
+            } else if (current.left == null) {
+                if (current == root) {
+                    root = current.right;
+                } else {
+                    if (isLeft) {
+                        parent.left = current.right;
+                    } else {
+                        parent.right = current.right;
+                    }
+                }
+            } else {
+                Node successor = GetSuccessor(current);
+
+                if (current == root) {
+                    root = successor;
+                } else if (isLeft) {
+                    parent.left = successor;
+                } else {
+                    parent.right = successor;
+                }
+            }
+
+        }
+
+        public void InOrder() {
+            if (root != null) { root.InOrder(); }
+        }
+        
+        public void PreOrder() {
+            if (root != null) { root.PreOrder(); }
+        }
+
+        public void PostOrder() {
+            if (root != null) { root.PostOrder(); }
+        }
+
+        private Node GetSuccessor(Node node) {
+            Node sParent = node;
+            Node successor = node;
+            Node current = node.right;
+
+            while (current != null) {
+                sParent = successor;
+                successor = sParent;
+                current = current.left;
+            }
+
+            if (successor != node.right) {
+                sParent.left = successor.right;
+                successor.right = node.right;
+            }
+
+            successor.left = node.left;
+
+            return successor;
+        }
+    }
+}
---
+++ Node.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using static System.Console;
+
+namespace BTrees
+{
+    public class Node
+    {
+        public Node left = null;
+        public Node right = null;
+        public int data;
+
+        public Node(int data) {
+            this.data = data;
+        }
+
+        public void Insert(int data) {
+            if (data >= this.data) {
+                if (right == null) {
+                    right = new Node(data);
+                } else {
+                    right.Insert(data);
+                }
+            } else {
+                if (left == null) {
+                    left = new Node(data);
+                } else {
+                    left.Insert(data);
+                }
+            }
+        }
+
+        public Node Find(int data) {
+            Node current = this;
+
+            while (current != null) {
+                if (data == current.data) {
+                    return current;
+                } else if (data > current.data) {
+                    current = current.right;
+                } else {
+                    current = current.left;
+                }
+            }
+
+            return null;
+        }
+
+        public Node FindRecursive(int data) {
+            if (data == this.data) {
+                return this;
+            } else if (data < this.data && left != null) {
+                return left.FindRecursive(data);
+            } else if (right != null) {
+                return right.FindRecursive(data);
+            } else {
+                return null;
+            }
+        }
+
+        public void InOrder() {
+            if (left != null) { left.InOrder(); }
+            WriteLine(data);
+            if (right != null) { right.InOrder(); }
+        }
+
+        public void PreOrder() {
+            WriteLine(data);
+            if (left != null) { left.PreOrder(); }
+            if (right != null) { right.PreOrder(); }
+        }
+
+        public void PostOrder() {
+            if (left != null) { left.PostOrder(); }
+            if (right != null) { right.PostOrder(); }
+            WriteLine(data);
+        }
+    }
+}
